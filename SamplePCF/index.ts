ebdf4f0ee4ff4291e96429edfff986b4f7cefe33
rexkenley <rexkenley@hotmail.com>
2020-01-07 10:00:29 -0500

    Create index.ts

---
+++ SamplePCF/index.ts
@@ -0,0 +1,100 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
+
+export class DatetimeBox
+  implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+  private container: HTMLDivElement;
+  private notifyOutputChanged: () => void;
+  private currentValue: object | null;
+  private updatedByReact: boolean;
+  /**
+   * Empty constructor.
+   */
+  constructor() {}
+
+  /**
+   * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+   * Data-set values are not initialized here, use updateView.
+   * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+   * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+   * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+   * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
+   */
+  public init(
+    context: ComponentFramework.Context<IInputs>,
+    notifyOutputChanged: () => void,
+    state: ComponentFramework.Dictionary,
+    container: HTMLDivElement
+  ) {
+    const { value } = context.parameters;
+
+    this.container = container;
+    this.notifyOutputChanged = notifyOutputChanged;
+    this.currentValue = (value && value.raw) || null;
+    this.updatedByReact = false;
+
+    // Add control initialization code
+    ReactDOM.render(
+      // @ts-ignore
+      React.createElement(SamplePCF, {
+        // @ts-ignore
+        value: this.currentValue,
+        onSampleChange: value => {
+          this.currentValue = value;
+          this.updatedByReact = true;
+          this.notifyOutputChanged();
+        }
+      }),
+      this.container
+    );
+  }
+
+  /**
+   * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+   * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+   */
+  public updateView(context: ComponentFramework.Context<IInputs>): void {
+    // Add code to update control view
+    const { value } = context.parameters;
+
+    if (this.updatedByReact) {
+      if (this.currentValue === value.raw) this.updatedByReact = false;
+
+      return;
+    }
+
+    this.currentValue = value.raw || null;
+
+    ReactDOM.render(
+      // @ts-ignore
+      React.createElement(SamplePCF, {
+        // @ts-ignore
+        value: this.currentValue,
+        onSampleChange: value => {
+          this.currentValue = value;
+          this.updatedByReact = true;
+          this.notifyOutputChanged();
+        }
+      }),
+      this.container
+    );    
+  }
+
+  /**
+   * It is called by the framework prior to a control receiving new data.
+   * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
+   */
+  public getOutputs(): IOutputs {
+    //@ts-ignore
+    return { value: this.currentValue };
+  }
+
+  /**
+   * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+   * i.e. cancelling any pending remote calls, removing listeners, etc.
+   */
+  public destroy(): void {
+    ReactDOM.unmountComponentAtNode(this.container);
+  }
+}
