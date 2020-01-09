aed9e8269b4ff8ea82e100fa8875c582214c054b
rexkenley <rexkenley@hotmail.com>
2020-01-08 20:19:45 -0500

    Update jest.stories.js

--- stories/jest.stories.js
+++ stories/jest.stories.js
@@ -3,9 +3,9 @@ import { storiesOf } from "@storybook/react";
 import { withTests } from "@storybook/addon-jest";
 
 import("../test/.jest-test-results.json").then(results => {
-  storiesOf("DatetimeBox", module)
+  storiesOf("Sample", module)
     .addDecorator(withTests({ results }))
     .add("Test Results", () => <div>Jest results in storybook</div>, {
-      jest: ["datetime.test.js", "storyShots.test.js"]
+      jest: ["storyShots.test.js"]
     });
 });
