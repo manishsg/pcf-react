f7b4238dc378046e434521d06da754afac6517b6
rexkenley <rexkenley@hotmail.com>
2020-01-13 10:53:01 -0500

    Update jest.stories.js

--- stories/jest.stories.js
+++ stories/jest.stories.js
@@ -2,10 +2,13 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withTests } from "@storybook/addon-jest";
 
-import("../test/.jest-test-results.json").then(results => {
-  storiesOf("Sample", module)
-    .addDecorator(withTests({ results }))
-    .add("Test Results", () => <div>Jest results in storybook</div>, {
+import("../test/__results__/.jest-test-results.json").then(results => {
+  storiesOf("Sample", module).add(
+    "Test Results",
+    () => <div>Jest results in storybook</div>,
+    {
+      decorators: [withTests({ results })],
       jest: ["storyShots.test.js"]
-    });
+    }
+  );
 });
